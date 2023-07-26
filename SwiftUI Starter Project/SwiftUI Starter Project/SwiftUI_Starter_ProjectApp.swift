82161ebaf81061d146b6569107695b6d36372d0e
Mark Jones <mjones@letsgetchecked.com>
2023-07-26 18:02:42 +0100

    Adding the Starter project

---
+++ SwiftUI Starter Project/SwiftUI Starter Project/SwiftUI_Starter_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Starter_ProjectApp.swift
+//  SwiftUI Starter Project
+//
+//  Created by Mark Jones on 26/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Starter_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
